317dceff507fb63db01e7eeab65588eef900dedf
qrenaud <quentin.renaud@epitech.eu>
2020-04-23 16:19:38 +0200

    Fix: remove fake

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,8 +1,3 @@
-data "aws_s3_bucket_object" "scheduler_template" {
-  bucket  = "solutions-reference"
-  key     = "aws-instance-scheduler/instance-scheduler.template"
-}
-
 resource "aws_cloudformation_stack" "scheduler" {
   name = "ec2-scheduler"
   template_body = file("${path.module}/deployment/instance-scheduler.template")
