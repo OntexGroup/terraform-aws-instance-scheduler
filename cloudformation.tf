da5bc3c81e63ef131c6d54162fa86ab835adc72b
qrenaud <quentin.renaud@epitech.eu>
2020-04-23 15:53:35 +0200

    Update: Tf files for cloudformation

---
+++ cloudformation.tf
@@ -0,0 +1,9 @@
+data "aws_s3_bucket_object" "scheduler_template" {
+  bucket  = "solutions-reference"
+  key     = "aws-instance-scheduler/instance-scheduler.template"
+}
+
+resource "aws_cloudformation_stack" "scheduler" {
+  name = "ec2-scheduler"
+  template_body = file("$(path.module}/deployment/instance-scheduler.template")
+}
