0ab3f862d2cae664e14f6a57dc957045ebb8b42d
qrenaud <quentin.renaud@epitech.eu>
2020-04-23 16:01:25 +0200

    Fix: add region variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "project" {
   type = string
   description = "Used to identify the project(s) the resource supports"
 }
+
+variable "region" {
+  type = string
+  description = "Used to set region"
+}
